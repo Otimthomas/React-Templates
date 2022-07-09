70e823a2dcf7048bb33533fcc7ea8968ec3de85a
unknown <tototim444@hotmail.com>
2022-07-09 21:01:04 +0300

    changed in app.tsx -2

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-      React Templating Application
-=======
       React Templating Application ---- changes made by thomas otim
->>>>>>> Stashed changes
     </div>
   );
 }
