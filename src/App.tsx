423ab61075c08f13b1f8e06cb55da048ec3ba936
unknown <tototim444@hotmail.com>
2022-07-09 20:38:33 +0300

    bug(app) - changes made by thomas

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      React Templating Application -> changes made by thomas
+      React Templating Application - changes made by thomas otim
     </div>
   );
 }
