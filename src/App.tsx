d7a186a7826e99871392fbd579fabfad786f1e9f
unknown <tototim444@hotmail.com>
2022-07-09 21:05:05 +0300

    thomas branch

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      React Templating Application - changes made by thomas otim
+      React Templating Application  changes made by thomas otim -thomas branch
     </div>
   );
 }
