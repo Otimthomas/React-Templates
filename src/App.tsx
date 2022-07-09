858697811ab53494bbaef7b19a437beb5cf26620
unknown <tototim444@hotmail.com>
2022-07-09 21:09:51 +0300

    thomas_2

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
 
-      React Templating Application -> changes made by thomas otim
+      React Templating Application - changes made by thomas otim
     </div>
   );
 }
